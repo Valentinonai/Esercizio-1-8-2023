fc9ea19255daac39600311cd1a43ce5e320a5b67
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-08-01 20:35:56 +0200

    v2.1

--- scrip.js
+++ scrip.js
@@ -13,9 +13,18 @@ document.getElementById("tot").innerText += document.getElementsByClassName("car
 
 let link = document.getElementsByClassName("link");
 link = Array.from(link);
-
 const cambiaLink = (event) => {
   if (event.target.innerText === "Nascondi sezione") event.target.innerText = "Mostra sezione";
   else if (event.target.innerText === "Mostra sezione") event.target.innerText = "Nascondi sezione";
 };
 link.forEach((x) => x.addEventListener("click", cambiaLink));
+
+//! VERSIONE ABBREVIATA
+/*
+document.querySelectorAll(".link").forEach((x) =>
+  x.addEventListener("click", (event) => {
+    if (event.target.innerText === "Nascondi sezione") event.target.innerText = "Mostra sezione";
+    else if (event.target.innerText === "Mostra sezione") event.target.innerText = "Nascondi sezione";
+  })
+);
+*/
